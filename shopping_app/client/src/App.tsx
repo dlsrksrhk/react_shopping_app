13dca2a2ec679025b6d4549f23a91f7d0accb2e6
sweet.dh <dlsrksrhk@gmail.com>
2025-03-16 13:26:31 +0900

    개발 완료

--- shopping_app/client/src/App.tsx
+++ shopping_app/client/src/App.tsx
@@ -2,15 +2,18 @@
 
 import { Route, Routes } from "react-router-dom";
 import { HomePage, ProductCreatePage, ProductPage } from "./pages";
+import { Layout } from "./components/shared";
 
 function App() {
-  
+
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="/:productId" element={<ProductPage />} />
-      <Route path="/create" element={<ProductCreatePage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/:productId" element={<ProductPage />} />
+        <Route path="/create" element={<ProductCreatePage />} />
+      </Routes>
+    </Layout>
   );
 }
 
