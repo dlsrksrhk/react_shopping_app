a495c6b51c48e52ba4ee89dc95bcedf90fe70a5e
sweet.dh <dlsrksrhk@gmail.com>
2025-03-16 17:58:12 +0900

    썸네일 업로드 구현 완료
    썸네일 등록, 및 목록페이지, 상세페이지에 썸네일 보이도록 적용

--- shopping_app/client/src/pages/ProductPage.tsx
+++ shopping_app/client/src/pages/ProductPage.tsx
@@ -1,12 +1,14 @@
 // ProductPage.tsx
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
 
 type ProductType = {
   id: string;
   name: string;
   explanation: string;
   price: number;
+  thumbnail?: string;
 }
 
 function ProductPage() {
@@ -17,15 +19,18 @@ function ProductPage() {
     fetch(`/product/${productId}`)
       .then((res) => res.json())
       .then((data) => setProduct(data.product));
-   }, [productId]);
+  }, [productId]);
 
-   if(!product) {
-      return <h1>존재하지 않는 상품입니다.</h1>;
-    }
+  if (!product) {
+    return <h1>존재하지 않는 상품입니다.</h1>;
+  }
 
   return (
     <div>
       <h1>{product?.name}</h1>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mb: 4 }}>
+        {product?.thumbnail && <img src={product?.thumbnail} alt={product?.name} style={{ width: "100%", maxWidth: 400 }} />}
+      </Box>
       <p>{product?.explanation}</p>
       <span>{product?.price}</span>
     </div>
